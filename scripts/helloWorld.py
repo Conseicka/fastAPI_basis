8ff639aaedfa006e7ec2f4cab79e32b53a5393dd
conseicka <>
2022-01-08 16:02:12 -0600

    validation model

--- scripts/helloWorld.py
+++ scripts/helloWorld.py
@@ -1,8 +1,10 @@
 #Python
 from typing import Optional
+from enum import Enum
 
 #Pydantic
 from pydantic import BaseModel
+from pydantic import Field
 
 #FastAPI
 from fastapi import FastAPI
@@ -11,6 +13,12 @@ from fastapi import Body, Query, Path
 #uvn helloWorld:app --reload
 app = FastAPI()
 
+class HairColor(Enum):
+    white = "white"
+    brown = "brown"
+    black = "black"
+    blonde = "blonde"
+    red ="red"
 
 class Location(BaseModel):
     city: str
@@ -21,11 +29,22 @@ class Location(BaseModel):
 #Models
 
 class Person(BaseModel):
-    first_name: str
-    last_name: str
-    age: int
-    hair_color: Optional[str] = None
-    is_married: Optional[bool] = None
+    first_name: str = Field(
+    ...,
+    min_length = 1,
+    max_length = 50,
+    )
+    last_name: str = Field(
+    ...,
+    min_length = 1,
+    max_length = 50
+    )
+    age: int = Field(
+    gt = 0,
+    le = 115
+    )
+    hair_color: Optional[HairColor] = Field(default = None)
+    is_married: Optional[bool] = Field(default = None)
 
 
 
